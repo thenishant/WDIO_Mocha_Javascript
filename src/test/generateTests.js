dc1fd6b95efffc75d474b6dc9b6bc086b0789d2c
|thenishant| <thenishant3@gmail.com>
2023-12-05 14:30:46 +0530

    Add timeout for 60 seconds

--- src/test/generateTests.js
+++ src/test/generateTests.js
@@ -1,50 +1,45 @@
-// generateTests.js
-
 import fs from 'fs';
 import path from 'path';
 
-function generateLabels(count) {
-    const labels = [];
-    for (let i = 1; i <= count; i++) {
-        labels.push(`label${i}`);
-    }
-    return labels;
-}
-
 const channels = ['channel1', 'channel2', 'channel3', 'channel4'];
-const labels = generateLabels(60);
 
+const labels = generateLabels(60);
 const currentModuleDir = path.dirname(new URL(import.meta.url).pathname);
 const generatedTestsDir = path.join(currentModuleDir, './generated');
 
+function generateLabels(count) {
+  const labels = [];
+  for (let i = 1; i <= count; i++) {
+    labels.push(`label${i}`);
+  }
+  console.log(`Created ${labels.length * channels.length} test files`)
+  return labels;
+}
+
 // Ensure that the directory exists
-fs.mkdirSync(generatedTestsDir, { recursive: true });
+fs.mkdirSync(generatedTestsDir, {recursive: true});
 
 channels.forEach((channel) => {
-    labels.forEach((label) => {
-        const testName = `Test suite for Channel @${channel}, Label ${label}`;
-        const testContent = `
+  labels.forEach((label) => {
+    const testName = `Test suite for Channel @${channel}, Label ${label}`;
+    const testContent = `
             import allure from '@wdio/allure-reporter';
-
             global.allure = allure;
 
             function sleep(ms) {
                 return new Promise(resolve => setTimeout(resolve, ms));
             }
 
-            describe("${testName}", () => {
-                it("should have your test logic", async() => {
-                    // Your test logic here
-                    await sleep(20000); // Sleep for 20 seconds
-                    console.log("Running test on channel: ${channel}, label: ${label}");
-                    const pageTitle = browser.getTitle();
-                    // assert.strictEqual(pageTitle, 'Your Expected Page Title');
-                });
-            });
+            describe("${testName}", async () => {
+              it("should have your test logic", async function () {
+                  await sleep(20000); // Sleep for 20 seconds
+                  console.log("Running test on channel: ${channel}, label: ${label}");
+              });
+          });
         `;
 
-        const filePath = path.join(generatedTestsDir, `${channel}_${label}.js`);
+    const filePath = path.join(generatedTestsDir, `${channel}_${label}.js`);
 
-        fs.writeFileSync(filePath, testContent);
-    });
+    fs.writeFileSync(filePath, testContent);
+  });
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -3,46 +3,50 @@ import allure from '@wdio/allure-reporter';
 global.allure = allure;
 let channel1 = 'Channel1';
 
-let testPath = 'test';
+let testPath = 'src/test/generated';
 export const config = {
-    runner: 'local',
-    specs: [`${testPath}/*.js`],
+  runner: 'local',
+  specs: [`${testPath}/*.js`],
 
-    suites: {
-        channel1: [
-          `${testPath}/channel1_*.js`,
-        ], channel2: [
-        `${testPath}/channel2_*.js`,
-        ], channel3: [
-        `${testPath}/channel3_*.js`,
-        ], channel4: [
-        `${testPath}/channel4_*.js`,
-        ],
-
-    },
-    maxInstances: 20,
-    capabilities: [
-        {
-            browserName: 'chrome',
-            acceptInsecureCerts: true,
-            'goog:chromeOptions': {
-                // binary: '/Users/nishant/Development/Personal/WDIO_Mocha_Javascript/chrome/mac_arm-119.0.6045.105/chrome-mac-arm64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing',
-                args: [
-                    '--no-sandbox',
-                    '--disable-infobars',
-                    '--disable-gpu',
-                    '--headless',
-                    '--window-size=1440,735'
-                ],
-            },
-        },
+  suites: {
+    channel1: [
+      `${testPath}/channel1_*.js`,
+    ], channel2: [
+      `${testPath}/channel2_*.js`,
+    ], channel3: [
+      `${testPath}/channel3_*.js`,
+    ], channel4: [
+      `${testPath}/channel4_*.js`,
     ],
-    // concurrency: 20,
 
-
-    before: function () {
+  },
+  maxInstances: 20,
+  framework: 'mocha',
+  mochaOpts: {
+    timeout: 60000
+  },
+  capabilities: [
+    {
+      browserName: 'chrome',
+      acceptInsecureCerts: true,
+      'goog:chromeOptions': {
+        // binary: '/Users/nishant/Development/Personal/WDIO_Mocha_Javascript/chrome/mac_arm-119.0.6045.105/chrome-mac-arm64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing',
+        args: [
+          '--no-sandbox',
+          '--disable-infobars',
+          '--disable-gpu',
+          '--headless',
+          '--window-size=1440,735'
+        ],
+      },
     },
+  ],
+  // concurrency: 20,
 
-    after: function () {
-    }
+
+  before: function () {
+  },
+
+  after: function () {
+  }
 };
