17452a11ee9c7aaadf37e3f80844c105bf4b1d7a
|thenishant| <thenishant3@gmail.com>
2023-12-05 13:40:02 +0530

    Add ls -al

--- src/resources/config.js
+++
@@ -1,6 +0,0 @@
-// ES Modules
-export const emailId = '<Enter valid test data>';
-
-export const username = 'Admin';
-
-export const password = 'admin123';
--- src/resources/constants.js
+++
@@ -1,8 +0,0 @@
-export const keys = {
-    ENTER: 'Enter',
-    ESCAPE: 'Escape',
-}
-
-export const search = {
-    SEARCH_KEYWORD: '<Enter the search data>',
-}
