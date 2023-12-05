07ecac6e3bd8bb9e04320e8ee1eff8de9b8d0998
|thenishant| <thenishant3@gmail.com>
2023-12-05 13:34:59 +0530

    Update file path

--- wdio.conf.js
+++ wdio.conf.js
@@ -5,44 +5,44 @@ let channel1 = 'Channel1';
 
 let testPath = '.generated-tests';
 export const config = {
-  runner: 'local',
-  specs: [`${testPath}/*.js`],
+    runner: 'local',
+    specs: [`${testPath}/*.js`],
 
-  suites: {
-    channel1: [
-      `${testPath}/channel1_*.js`,
-    ], channel2: [
-      `${testPath}/channel2_*.js`,
-    ], channel3: [
-      `${testPath}/channel3_*.js`,
-    ], channel4: [
-      `${testPath}/channel4_*.js`,
-    ],
-
-  },
-  maxInstances: 20,
-  capabilities: [
-    {
-      browserName: 'chrome',
-      acceptInsecureCerts: true,
-      'goog:chromeOptions': {
-        // binary: '/Users/nishant/Development/Personal/WDIO_Mocha_Javascript/chrome/mac_arm-119.0.6045.105/chrome-mac-arm64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing',
-        args: [
-          '--no-sandbox',
-          '--disable-infobars',
-          '--disable-gpu',
-          '--headless',
-          '--window-size=1440,735'
+    suites: {
+        channel1: [
+          `${testPath}/channel1_*.js`,
+        ], channel2: [
+        `${testPath}/channel2_*.js`,
+        ], channel3: [
+        `${testPath}/channel3_*.js`,
+        ], channel4: [
+        `${testPath}/channel4_*.js`,
         ],
-      },
+
     },
-  ],
-  // concurrency: 20,
+    maxInstances: 20,
+    capabilities: [
+        {
+            browserName: 'chrome',
+            acceptInsecureCerts: true,
+            'goog:chromeOptions': {
+                // binary: '/Users/nishant/Development/Personal/WDIO_Mocha_Javascript/chrome/mac_arm-119.0.6045.105/chrome-mac-arm64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing',
+                args: [
+                    '--no-sandbox',
+                    '--disable-infobars',
+                    '--disable-gpu',
+                    '--headless',
+                    '--window-size=1440,735'
+                ],
+            },
+        },
+    ],
+    // concurrency: 20,
 
 
-  before: function () {
-  },
+    before: function () {
+    },
 
-  after: function () {
-  }
+    after: function () {
+    }
 };
